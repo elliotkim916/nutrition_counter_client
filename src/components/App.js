6e5129450cc256af7af4f0544bd3344e69d54436
Elliot Kim <elliotkim916@gmail.com>
2018-09-11 18:17:07 -0700

    Added import of LandingPage component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import LandingPage from './landing-page';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-       
+        <LandingPage/>
       </div>
     );
   }
