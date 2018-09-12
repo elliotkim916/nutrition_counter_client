f09cb7c8332f945b32ee30b928c1b3a47e4364cd
Elliot Kim <elliotkim916@gmail.com>
2018-09-11 18:16:18 -0700

    Added landing page component with text description and button

---
+++ src/components/landing-page.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './landing-page.css';
+
+export default class LandingPage extends React.Component {
+  render() {
+    return (
+      <div className='landing-page'>
+        <h1>Nutrition Counter</h1>
+
+        <h4>Enter your meal and see the nutrition information of your entire meal broken down into calories, fats, carbohydrates, and more!</h4>
+
+        <h4>See your meal broken down by ingredients and see an image and the nutrition of each specific ingredient.</h4>
+
+        <h4>Enter your workout and the duration of your workout and get back a best estimation of how many calories you burned.</h4>
+
+        <button type="submit">Begin Now</button>
+      </div>
+    );
+  }
+}
