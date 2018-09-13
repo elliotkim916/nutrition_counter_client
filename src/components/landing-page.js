f7948db9910ba5f04757665456b54d417beb9f36
Elliot Kim <elliotkim916@gmail.com>
2018-09-12 18:06:25 -0700

    Added onClick event handler on button

--- src/components/landing-page.js
+++ src/components/landing-page.js
@@ -4,7 +4,7 @@ import './landing-page.css';
 export default class LandingPage extends React.Component {
   render() {
     return (
-      <div className='landing-page'>
+      <div className="landing-page">
         <h1>Nutrition Counter</h1>
 
         <h4>Enter your meal and see the nutrition information of your entire meal broken down into calories, fats, carbohydrates, and more!</h4>
@@ -13,7 +13,12 @@ export default class LandingPage extends React.Component {
 
         <h4>Enter your workout and the duration of your workout and get back a best estimation of how many calories you burned.</h4>
 
-        <button type="submit">Begin Now</button>
+        <button 
+          type="submit" 
+          onClick={() => this.props.toSearchPage()}
+        >
+        Begin Now
+        </button>
       </div>
     );
   }
