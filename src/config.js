021b702af32cfcd20a37ed472e89eca36ac4f536
Elliot Kim <elliotkim916@gmail.com>
2018-09-14 17:52:48 -0700

    Added nutritionix API KEY, ID, and BASE URL

---
+++ src/config.js
@@ -0,0 +1,3 @@
+export const NUTRITIONIX_BASE_URL = "https://trackapi.nutritionix.com/v2/natural/nutrients";
+export const APP_ID = "aeff1b26";
+export const APP_KEY = "96ef74633a183f547e97d2dc6c8819d4";
