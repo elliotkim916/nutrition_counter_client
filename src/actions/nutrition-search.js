7b2df64b640eab0d0b79169cdb6a4c0d0d9a9c7d
Elliot Kim <elliotkim916@gmail.com>
2018-09-14 17:56:14 -0700

    Added   - Nutritionix API key and URL   - fetchNutrition function   - get_nutrition async action

--- src/actions/nutrition-search.js
+++ src/actions/nutrition-search.js
@@ -1,3 +1,9 @@
+import {
+  NUTRITIONIX_BASE_URL,
+  APP_ID,
+  APP_KEY
+} from '../config';
+
 export const NUTRITION_SEARCH_REQUEST = 'NUTRITION_SEARCH_REQUEST';
 export const nutrtionSearchRequest = () => ({
   type: NUTRITION_SEARCH_REQUEST
@@ -14,3 +20,38 @@ export const nutritionSearchError = error => ({
   type: NUTRITION_SEARCH_ERROR,
   error
 });
+
+function fetch_nutrition(meal) {
+  const headers = {
+    'Content-Type': 'application/json',
+    'x-app-id': `${APP_ID}`,
+    'x-app-key': `${APP_KEY}`
+  }
+
+  const body = {
+    'query': meal,
+    'timezone': 'US/Eastern'
+  }
+
+  return fetch(`${NUTRITIONIX_BASE_URL}`, {
+    method: 'POST',
+    body: JSON.stringify(meal),
+    headers: headers
+  }).then(res => {
+    if (!res.ok) {
+      return Promise.reject(res.statusText);
+    }
+    return res.json();
+  }).then(data => {
+    console.log(data);
+  });
+}
+
+export const get_nutrition = meal => dispatch => {
+  dispatch(nutrtionSearchRequest());
+  fetch_nutrition(meal).then(meal => {
+    dispatch(nutritionSearchSuccess(meal))
+  }).catch(error => {
+    dispatch(nutritionSearchError(error));
+  });
+} 
