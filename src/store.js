2155412d46b5a6e3d8fd141f34e95a5ae9199e2c
Elliot Kim <elliotkim916@gmail.com>
2018-09-22 19:29:10 -0700

    Added exerciseSearchReducer

--- src/store.js
+++ src/store.js
@@ -2,11 +2,13 @@ import {createStore, applyMiddleware, combineReducers} from 'redux';
 import thunk from 'redux-thunk';
 import {beginNowReducer} from './reducers/begin-now';
 import {nutritionSearchReducer} from './reducers/nutrition-search';
+import {exerciseSearchReducer} from './reducers/exercise-search';
 
 const store = createStore(
   combineReducers({
     begin: beginNowReducer, 
-    nutrition: nutritionSearchReducer
+    nutrition: nutritionSearchReducer,
+    exercise: exerciseSearchReducer
   }),
   applyMiddleware(thunk)
 );
