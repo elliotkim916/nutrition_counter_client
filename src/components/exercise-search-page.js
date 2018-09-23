ac99269787e0ad9397460796af6b97d7b79498a9
Elliot Kim <elliotkim916@gmail.com>
2018-09-22 19:34:08 -0700

    Added   - connect   - onSubmit event handler to form   - dispatch of get_exercise async action   - ref to textarea
    Removed
      - onClick event handler on button

--- src/components/exercise-search-page.js
+++ src/components/exercise-search-page.js
@@ -1,26 +1,32 @@
 import React from 'react';
 import './exercise-search-page.css';
+import {connect} from 'react-redux';
+import {get_exercise} from '../actions/exercise-search';
 
-export default class ExerciseSearchPage extends React.Component {
+export class ExerciseSearchPage extends React.Component {
   calculateActivity(e) {
     e.preventDefault();
-    console.log('clicked');
+    this.props.dispatch(get_exercise(this.input.value));
+    this.input.value = '';
   }
 
   render() {
     return (
       <div className="exercise-page">
-        <form>
+        <form onSubmit={(e) => this.calculateActivity(e)}>
           <label htmlFor="exercise">Enter your workout.  It can be anything from 30 min of weight lifting, 1 hour of yoga, or a 2 hour walk!</label><br/>
           <textarea 
             id="exercise"
             type="textarea" 
             placeholder="ran 3 miles"
+            ref={input => this.input = input}
           >
           </textarea><br/>
-          <button type="submit" onClick={(e) => this.calculateActivity(e)}>Calculate Activity</button>
+          <button type="submit">Calculate Activity</button>
         </form>
       </div>
     );
   }
 }
+
+export default connect()(ExerciseSearchPage);
