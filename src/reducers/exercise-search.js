4d23228a6a46d226c40bbb934db5e7df88ef3cb8
Elliot Kim <elliotkim916@gmail.com>
2018-09-22 19:02:37 -0700

    Added   - initialState   - import of actions   - exerciseSearchReducer function

---
+++ src/reducers/exercise-search.js
@@ -0,0 +1,29 @@
+import {
+  EXERCISE_SEARCH_REQUEST,
+  EXERCISE_SEARCH_SUCCESS,
+  EXERCISE_SEARCH_ERROR
+} from '../actions/exercise-search';
+
+const initialState = {
+  loading: false,
+  exercise_results: [],
+  error: null
+};
+
+export default function exerciseSearchReducer(state=initialState, action) {
+  if (action.type === EXERCISE_SEARCH_REQUEST) {
+    return Object.assign({}, state, {
+      loading: true
+    });
+  } else if (action.type === EXERCISE_SEARCH_SUCCESS) {
+    return Object.assign({}, state, {
+      loading: false,
+      results: action.exercise_results
+    });
+  } else if (action.type === EXERCISE_SEARCH_ERROR) {
+    return Object.assign({}, state, {
+      error: action.error
+    });
+  }
+  return state;
+} 
