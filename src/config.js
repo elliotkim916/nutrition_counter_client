8c4214e5d77aa9af3afee8e17420e5a94d396248
Elliot Kim <elliotkim916@gmail.com>
2018-09-22 19:28:48 -0700

    Added exercise base url

--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
 export const NUTRITIONIX_BASE_URL = "https://trackapi.nutritionix.com/v2/natural/nutrients";
+export const EXERCISE_BASE_URL = "https://trackapi.nutritionix.com/v2/natural/exercise";
 export const APP_ID = "aeff1b26";
 export const APP_KEY = "96ef74633a183f547e97d2dc6c8819d4";
