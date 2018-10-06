b4b8ac126e9708dc7b09475872398b26411f5f08
Elliot Kim <elliotkim916@gmail.com>
2018-10-05 17:36:48 -0700

    Added ExerciseResults component

--- src/components/App.js
+++ src/components/App.js
@@ -5,10 +5,10 @@ import LandingPage from './landing-page';
 import NutritionSearchPage from './nutrition-search-page';
 import ExerciseSearchPage from './exercise-search-page';
 import NutritionSearchResults from './nutrition-search-results';
+import ExerciseResults from './exercise-results';
 
 export class App extends Component {
   render() {
-    console.log(this.props);
     const connect = [];
 
     if (this.props.start.show_landing_page) {
@@ -16,8 +16,9 @@ export class App extends Component {
     } else if (this.props.start.show_nutrition_search_page && this.props.start.show_exercise_search_page) {
       connect.push(
         <NutritionSearchPage key="NutritionSearchPage"/>,
-        <NutritionSearchResults key="NutritionSearchResults" />, 
-        <ExerciseSearchPage key="ExerciseSearchPage"/>
+        <NutritionSearchResults key="NutritionSearchResults"/>, 
+        <ExerciseSearchPage key="ExerciseSearchPage"/>,
+        <ExerciseResults key="ExerciseResults"/>
       );
     } else {
       console.log('error');
@@ -32,8 +33,7 @@ export class App extends Component {
 }
 
 const mapStateToProps = state => ({
-  start: state.begin,
-  nutrition: state.nutrition
+  start: state.begin
 });
 
 export default connect(mapStateToProps)(App);
