25f9d5d160f74f8c94c189157218516a92c9229f
Elliot Kim <elliotkim916@gmail.com>
2018-10-13 16:40:21 -0700

    Added   - Router, Route, and history
    Removed
     - connect array
     - conditional logic that pushes components into connect array, which is rendered

--- src/components/App.js
+++ src/components/App.js
@@ -1,39 +1,22 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
 import './App.css';
+import { Router, Route } from 'react-router-dom';
+import history from '../history';
 import LandingPage from './landing-page';
-import NutritionSearchPage from './nutrition-search-page';
-import ExerciseSearchPage from './exercise-search-page';
-import NutritionSearchResults from './nutrition-search-results';
-import ExerciseResults from './exercise-results';
+import SearchPage from './search-page';
 
 export class App extends Component {
   render() {
-    const connect = [];
-
-    if (this.props.start.show_landing_page) {
-      connect.push(<LandingPage key="LandingPage"/>);
-    } else if (this.props.start.show_nutrition_search_page && this.props.start.show_exercise_search_page) {
-      connect.push(
-        <NutritionSearchPage key="NutritionSearchPage"/>,
-        <NutritionSearchResults key="NutritionSearchResults"/>, 
-        <ExerciseSearchPage key="ExerciseSearchPage"/>,
-        <ExerciseResults key="ExerciseResults"/>
-      );
-    } else {
-      console.log('error');
-    }
-
     return (
-      <div className="App">
-        {connect}
-      </div>
+      <Router history={history}>
+        <div className="App">
+          <Route exact path="/" component={LandingPage} />
+          <Route exact path="/nutrition-search" component={SearchPage}/>
+        </div>
+      </Router>
     );
   }
 }
 
-const mapStateToProps = state => ({
-  start: state.begin
-});
-
-export default connect(mapStateToProps)(App);
+export default connect()(App);
