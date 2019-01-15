67ede4abcb5301ae795e2c94b3054de3b076109e
Elliot Kim <elliotkim916@gmail.com>
2019-01-14 18:10:01 -0800

    Added formReducer

--- src/store.js
+++ src/store.js
@@ -2,11 +2,13 @@ import {createStore, applyMiddleware, combineReducers} from 'redux';
 import thunk from 'redux-thunk';
 import {nutritionSearchReducer} from './reducers/nutrition-search';
 import {exerciseSearchReducer} from './reducers/exercise-search';
+import {reducer as formReducer} from 'redux-form';
 
 const store = createStore(
   combineReducers({
     nutrition: nutritionSearchReducer,
-    exercise: exerciseSearchReducer
+    exercise: exerciseSearchReducer,
+    form: formReducer
   }),
   applyMiddleware(thunk)
 );
