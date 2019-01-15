a7caf8fd3cdc78295f392299090cf245441f982d
Elliot Kim <elliotkim916@gmail.com>
2019-01-14 19:23:48 -0800

    Added input component, with error & warning

---
+++ src/components/input.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export default class Input extends React.Component {
+  render() {
+    let error;
+    if (this.props.meta.touched && this.props.meta.error) {
+      error = <div className="form-error">{this.props.meta.error}</div>
+    }
+
+    let warning;
+    if (this.props.meta.touched && this.props.meta.warning) {
+      warning = (
+        <div className="form-warning">{this.props.meta.warning}</div>
+      );
+    }
+    
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
