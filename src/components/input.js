5a2d468f3dc92b735cb37b6312c2156beb8c9190
Elliot Kim <elliotkim916@gmail.com>
2019-01-16 17:38:23 -0800

    Added input and label

--- src/components/input.js
+++ src/components/input.js
@@ -16,7 +16,17 @@ export default class Input extends React.Component {
     
     return (
       <div className="form-input">
-      
+        <input
+          {...this.props.input}
+          id = {this.props.input.name}
+          type = {this.props.type}
+          ref = {input => (this.input = input)}
+          placeholder = {this.props.label}
+        />
+        <label htmlFor = {this.props.input.name}>
+          {error}
+          {warning}
+        </label>
       </div>
     );
   }
