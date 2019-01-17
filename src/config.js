7c15c3fa7b9d32e98bf7ef53c85bdec036cc866b
Elliot Kim <elliotkim916@gmail.com>
2019-01-16 18:01:37 -0800

    Added API_BASE_URL for server side

--- src/config.js
+++ src/config.js
@@ -2,3 +2,6 @@ export const NUTRITIONIX_BASE_URL = "https://trackapi.nutritionix.com/v2/natural
 export const EXERCISE_BASE_URL = "https://trackapi.nutritionix.com/v2/natural/exercise";
 export const APP_ID = "aeff1b26";
 export const APP_KEY = "96ef74633a183f547e97d2dc6c8819d4";
+
+// server side
+export const API_BASE_URL = process.env.API_BASE_URL || 'http://localhost:8080/api';
