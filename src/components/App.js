bc6d1b2d42e96f9a6bfe46cc173e929c0b8ddace
Elliot Kim <elliotkim916@gmail.com>
2019-01-18 17:30:04 -0800

    Added route for RegistrationPage

--- src/components/App.js
+++ src/components/App.js
@@ -5,16 +5,20 @@ import { Router, Route } from 'react-router-dom';
 import history from '../history';
 import LandingPage from './landing-page';
 import SearchPage from './search-page';
+import RegistrationPage from './registration-page';
+// import Practice from './practice';
+// import ApiCall from './apicall-practice';
 
 export class App extends Component {
   render() {
     return (
-      <Router history={history}>
+       <Router history={history}>
         <div className="App">
+          <Route exact path="/registration-page" component={RegistrationPage} />
           <Route exact path="/" component={LandingPage} />
           <Route exact path="/nutrition-search" component={SearchPage}/>
         </div>
-      </Router>
+       </Router> 
     );
   }
 }
