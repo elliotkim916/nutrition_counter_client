a1fe235b0c2664e2828da59673177414f48ebe58
Elliot Kim <elliotkim916@gmail.com>
2019-01-19 17:09:09 -0800

    Added   - import of API_BASE_URL, normalizeResponseErrors, SubmissionError   - login async action

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,3 +1,7 @@
+import {API_BASE_URL} from '../config';
+import {normalizeResponseErrors} from './utils';
+import {SubmissionError} from 'redux-form';
+
 export const AUTH_REQUEST = 'AUTH_REQUEST';
 export const authRequest = () => ({
   type: AUTH_REQUEST
@@ -14,3 +18,22 @@ export const authError = error => ({
   type: AUTH_ERROR,
   error
 });
+
+export const login = (username, password) => dispatch => {
+  dispatch(authRequest());
+  return (
+    fetch(`${API_BASE_URL}/auth/login`, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify({
+        username,
+        password
+      })
+    })
+    .then(res => normalizeResponseErrors(res))
+    .then(res => res.json())
+    .then()
+  )
+}
