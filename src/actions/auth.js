15c30a7bfafdc2303871572075536e5c21b9e355
Elliot Kim <elliotkim916@gmail.com>
2019-01-19 16:42:36 -0800

    Added auth request, success, & error actions

---
+++ src/actions/auth.js
@@ -0,0 +1,16 @@
+export const AUTH_REQUEST = 'AUTH_REQUEST';
+export const authRequest = () => ({
+  type: AUTH_REQUEST
+});
+
+export const AUTH_SUCCESS = 'AUTH_SUCCESS';
+export const authSuccess = currentUser => ({
+  type: AUTH_SUCCESS,
+  currentUser
+});
+
+export const AUTH_ERROR = 'AUTH_ERROR';
+export const authError = error => ({
+  type: AUTH_ERROR,
+  error
+});
