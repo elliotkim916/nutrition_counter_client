4d1bcfe5fb00f759f5c89ad0d4cba3bc49573dd0
Elliot Kim <elliotkim916@gmail.com>
2019-01-23 17:08:13 -0800

    Added protectedDataReducer to store

--- src/store.js
+++ src/store.js
@@ -2,6 +2,7 @@ import {createStore, applyMiddleware, combineReducers} from 'redux';
 import thunk from 'redux-thunk';
 import {nutritionSearchReducer} from './reducers/nutrition-search';
 import {exerciseSearchReducer} from './reducers/exercise-search';
+import {protectedDataReducer} from './reducers/protected-data';
 import {reducer as formReducer} from 'redux-form';
 import authReducer from './reducers/auth';
 
@@ -10,7 +11,8 @@ const store = createStore(
     nutrition: nutritionSearchReducer,
     exercise: exerciseSearchReducer,
     form: formReducer,
-    auth: authReducer
+    auth: authReducer,
+    protected: protectedDataReducer
   }),
   applyMiddleware(thunk)
 );
