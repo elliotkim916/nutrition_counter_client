176840b701daca619afa649c318073be69fd196a
Elliot Kim <elliotkim916@gmail.com>
2019-01-23 17:26:05 -0800

    Added basic dashboard for now

---
+++ src/components/dashboard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export class Dashboard extends React.Component {
+  render() {
+    return (
+      <div className="dashboard">
+        <h1>Welcome user!</h1>
+      </div>
+    )
+  }
+}
