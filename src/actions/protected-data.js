fc9fd2451672b8d9ff30c7c737057babac9f0b91
Elliot Kim <elliotkim916@gmail.com>
2019-01-23 17:07:39 -0800

    Added fetchProtectedData action creators for both success & error

---
+++ src/actions/protected-data.js
@@ -0,0 +1,15 @@
+import {API_BASE_URL} from '../config.js';
+import {normalizeResponseErrors} from './utils';
+
+export const FETCH_PROTECTED_DATA_SUCCESS = 'FETCH_PROTECTED_DATA_SUCCESS';
+export const fetchProtectedDataSuccess = data => ({
+  type: FETCH_PROTECTED_DATA_SUCCESS,
+  data
+});
+
+export const FETCH_PROTECTED_DATA_ERROR = 'FETCH_PROTECTED_DATA_ERROR';
+export const fetchProtectedDataError = error => ({
+  type: FETCH_PROTECTED_DATA_ERROR,
+  error
+});
+
