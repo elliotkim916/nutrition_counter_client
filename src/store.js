2ea9ab218ac85eb9dbeb48fe82c5af2bc12161ae
Elliot Kim <elliotkim916@gmail.com>
2019-02-01 22:10:51 -0800

    Enabled persistance, imported setAuthToken and refreshAuthToken

--- src/store.js
+++ src/store.js
@@ -5,6 +5,8 @@ import {exerciseSearchReducer} from './reducers/exercise-search';
 import {protectedDataReducer} from './reducers/protected-data';
 import {reducer as formReducer} from 'redux-form';
 import authReducer from './reducers/auth';
+import {loadAuthToken} from './local-storage';
+import {setAuthToken, refreshAuthToken} from './actions/auth';
 
 const store = createStore(
   combineReducers({
@@ -16,5 +18,14 @@ const store = createStore(
   }),
   applyMiddleware(thunk)
 );
+
+// With the JWT stored, for persistance to work is to reload the token when the application loads
+// this is done in the store
+const authToken = loadAuthToken();
+if (authToken) {
+  const token = authToken;
+  store.dispatch(setAuthToken(token));
+  store.dispatch(refreshAuthToken());
+}
 
 export default store;
