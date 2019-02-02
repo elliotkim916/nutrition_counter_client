3a538e355487eb00823672f4066e182ba45ab819
Elliot Kim <elliotkim916@gmail.com>
2019-02-01 21:27:38 -0800

    Added localStorage functions

---
+++ src/local-storage.js
@@ -0,0 +1,18 @@
+// In Safari's incognito mode, attempting to save a value to localStorage 
+// will throw an exception, so we wrap the call in a try statement
+
+export const loadAuthToken = () => {
+  return localStorage.getItem('authToken');
+}
+
+export const saveAuthToken = authToken => {
+  try {
+    localStorage.setItem('authToken', authToken);
+  } catch(e) {}
+};
+
+export const clearAuthToken = authToken => {
+  try {
+    localStorage.removeItem('authToken');
+  } catch(e) {}
+};
