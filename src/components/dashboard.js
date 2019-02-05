7041421f2bbabc49d7a51be8d904ddd952e68809
Elliot Kim <elliotkim916@gmail.com>
2019-02-04 19:06:26 -0800

    Added logOut function that clears users authToken

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import {clearAuth} from '../actions/auth';
+import {clearAuthToken} from '../local-storage';
 
 export class Dashboard extends React.Component {
+  logOut(e) {
+    e.preventDefault();
+    this.props.dispatch(clearAuth());
+    clearAuthToken();
+  }
+
   render() {
     return (
       <div className="dashboard">
+      <a 
+        href = "/"
+        className = "log-out"
+        onClick = {(e) => this.logOut(e)}
+      >
+      Log Out
+      </a>
         <h1>Welcome user!</h1>
       </div>
     )
