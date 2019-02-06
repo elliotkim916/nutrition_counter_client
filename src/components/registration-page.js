6f49021151b57a808d1451a7c7df063ec8c89efc
Elliot Kim <elliotkim916@gmail.com>
2019-02-05 17:53:41 -0800

    Added   - import of connect, Redirect, and Link   - mapStateToProps   - conditional statements for props.loading and props.loggedIn

--- src/components/registration-page.js
+++ src/components/registration-page.js
@@ -1,19 +1,33 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import {Redirect, Link} from 'react-router-dom';
 import RegistrationForm from './registration-form';
 
-export default function RegistrationPage(props) {
+export function RegistrationPage(props) {
   if (props.loggedIn) {
-    return;
+    return <Redirect to="/dashboard" />;
   }
 
   if (props.loading) {
-    return;
+    return (
+      <div className="loader">
+        <h3>Creating Account . . . </h3>
+      </div>
+    );
   }
 
   return (
     <div>
       <h3>Begin tracking your workout and meals today!</h3>
       <RegistrationForm/>
+      <span>Already have an account?  <Link to="">Click Here!</Link></span>
     </div>
   );
 }
+
+const mapStateToProps = state => ({
+  loggedIn: state.auth.currentUser !== null,
+  loading: state.auth.loading
+});
+
+export default connect(mapStateToProps)(RegistrationPage);
