de2b4c79b67fe71d9581d392037113b8148a71af
Elliot Kim <elliotkim916@gmail.com>
2019-02-05 17:59:47 -0800

    Added comments for more clarity

--- src/components/requires-login.js
+++ src/components/requires-login.js
@@ -4,10 +4,13 @@ import {Redirect} from 'react-router-dom';
 
 // A Higher-order component - used to add behavior to existing components by wrapping them in an outer component
 // HOC's are tools to facilitate code reuse amongst unrelated components
-
 // a function that returns another function that returns the wrapping React component RequiresLogin
+
 export default () => Component => {
+  // export default : I can name it whatever I want
+  // just export without default : Must respect already given name
   function RequiresLogin(props) {
+    // passThroughProps is coming from the component wrapped by the HOC
     const {authenticating, loggedIn, error, ...passThroughProps} = props;
     if (authenticating) {
       return <div>Logging in...</div>;
@@ -16,6 +19,11 @@ export default () => Component => {
     }
     return <Component {...passThroughProps} />;
   }
+
+  // why is there a string Component for the display name? and string for line 22?
+  const displayName = Component.displayName || Component.name || 'Component';
+  // Is RequiresLogin an object?
+  RequiresLogin.displayName = `RequiresLogin(${displayName})`;
 
   const mapStateToProps = (state, props) => ({
     authenticating: state.auth.loading,
