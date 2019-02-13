acf261d02a5437f8d1e8fb3bf359528f2f44e0ae
Elliot Kim <elliotkim916@gmail.com>
2019-02-12 18:33:29 -0800

    Added handler for if action.type is ADD_PROTECTED_DATA

--- src/reducers/protected-data.js
+++ src/reducers/protected-data.js
@@ -1,15 +1,18 @@
 import {
   FETCH_PROTECTED_DATA_SUCCESS,
-  FETCH_PROTECTED_DATA_ERROR
+  FETCH_PROTECTED_DATA_ERROR,
+  ADD_PROTECTED_DATA
 } from '../actions/protected-data';
 
 const initialState = {
   loading: false,
   error: null,
-  protected_data: ''
+  protected_data: '',
+  addedProtectedData: ''
 }
 
 export function protectedDataReducer(state=initialState, action) {
+  console.log(state);
   if (action.type === FETCH_PROTECTED_DATA_SUCCESS) {
     return Object.assign({}, state, {
       protected_data: action.data,
@@ -20,6 +23,12 @@ export function protectedDataReducer(state=initialState, action) {
     return Object.assign({}, state, {
       error: action.error,
       loading: false,
+    });
+  } else if (action.type === ADD_PROTECTED_DATA) {
+    return Object.assign({}, state, {
+      addedProtectedData: action.addedProtectedData,
+      loading: false,
+      error: null
     });
   }
   return state;
