c3aa15f01360e8c8e6d9a16337d4cc6dd007b409
Elliot Kim <elliotkim916@gmail.com>
2019-02-21 19:23:20 -0800

    Cleaned up code and fixed protected_data for ADD_PROTECTED_DATA

--- src/reducers/protected-data.js
+++ src/reducers/protected-data.js
@@ -11,39 +11,29 @@ const initialState = {
   error: null
 }
 
+// state is immutable in react/redux
+// directly modifying vs making a copy
 export function protectedDataReducer(state=initialState, action) {
-  if (action.type === FETCH_PROTECTED_DATA_SUCCESS) {
-    console.log('action.data for fetchTest', action.data);
-    console.log('state for fetchTest', state);
-    // how is action.data an array?
-    const fetchTest = Object.assign({}, state, {
+  if (action.type === FETCH_PROTECTED_DATA_SUCCESS) {  
+    return Object.assign({}, state, {
       protected_data: action.data,
       loading: false,
       error: null
     });
-    console.log('fetchTest is', fetchTest);
-    return fetchTest;
   } else if (action.type === ADD_PROTECTED_DATA) {
-    console.log('action for addTest', action);
-    console.log('state for addTest', state);
-    const addTest = Object.assign({}, state, {
-      protected_data: state.protected_data.push(action.addedProtectedData),
+    console.log(state);
+    return Object.assign({}, state, {
+      protected_data: [...state.protected_data, action.addedProtectedData],
       loading: false,
       error: null
     });
-    console.log('addTest is', addTest);
-    return addTest;
   } else if (action.type === DELETE_PROTECTED_DATA) {
-    console.log('action for deleteTest', action);
-    console.log('state for deleteTest', state);
     // Why are we using state.protected_data?
-    const deleteTest = Object.assign({}, state, {
+    return Object.assign({}, state, {
       protected_data: state.protected_data.filter(nutrition => nutrition._id !== action.id),
       loading: false,
       error: null
     });
-    console.log('deleteTest is', deleteTest);
-    return deleteTest;
   } else if (action.type === FETCH_PROTECTED_DATA_ERROR) {
     return Object.assign({}, state, {
       error: action.error,
