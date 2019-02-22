185ffcd15fd743a6a8069bbd3363ba020dea32f1
Elliot Kim <elliotkim916@gmail.com>
2019-02-21 17:43:24 -0800

    Added console logs for testing and updated protected_data for both delete and add

--- src/reducers/protected-data.js
+++ src/reducers/protected-data.js
@@ -13,29 +13,42 @@ const initialState = {
 
 export function protectedDataReducer(state=initialState, action) {
   if (action.type === FETCH_PROTECTED_DATA_SUCCESS) {
-    return Object.assign({}, state, {
-      protected_data:  action.data,
+    console.log('action.data for fetchTest', action.data);
+    console.log('state for fetchTest', state);
+    // how is action.data an array?
+    const fetchTest = Object.assign({}, state, {
+      protected_data: action.data,
       loading: false,
       error: null
     });
+    console.log('fetchTest is', fetchTest);
+    return fetchTest;
+  } else if (action.type === ADD_PROTECTED_DATA) {
+    console.log('action for addTest', action);
+    console.log('state for addTest', state);
+    const addTest = Object.assign({}, state, {
+      protected_data: state.protected_data.push(action.addedProtectedData),
+      loading: false,
+      error: null
+    });
+    console.log('addTest is', addTest);
+    return addTest;
+  } else if (action.type === DELETE_PROTECTED_DATA) {
+    console.log('action for deleteTest', action);
+    console.log('state for deleteTest', state);
+    // Why are we using state.protected_data?
+    const deleteTest = Object.assign({}, state, {
+      protected_data: state.protected_data.filter(nutrition => nutrition._id !== action.id),
+      loading: false,
+      error: null
+    });
+    console.log('deleteTest is', deleteTest);
+    return deleteTest;
   } else if (action.type === FETCH_PROTECTED_DATA_ERROR) {
     return Object.assign({}, state, {
       error: action.error,
       loading: false,
     });
-  } else if (action.type === ADD_PROTECTED_DATA) {
-    return Object.assign({}, state, {
-      protected_data: state.protected_data.push(action.addedProtectedData),
-      loading: false,
-      error: null
-    });
-  } else if (action.type === DELETE_PROTECTED_DATA) {
-    // Why are we using state.protected_data?
-    return Object.assign({}, state, {
-      protected_data: state.protected_data.filter(nutrition => nutrition._id !== action.id),
-      loading: false,
-      error: null
-    });
-  }
+  } 
   return state;
 }
