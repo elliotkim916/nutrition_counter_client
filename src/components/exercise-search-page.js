433ef406861a6b77d02225d6fe3b8da10165ad7a
Elliot Kim <elliotkim916@gmail.com>
2019-02-22 15:17:09 -0800

    Added ExerciseSearchPage component

---
+++ src/components/exercise-search-page.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import {get_exercise} from '../actions/exercise-search';
+import ExerciseResults from './exercise-results';
+
+export class ExerciseSearchPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      exercise_value : ''
+    }
+  }
+  
+  exerciseInput(e) {
+    this.setState({exercise_value: e.target.value});
+  }  
+
+  calculateActivity(e) {
+    e.preventDefault();
+    this.props.dispatch(get_exercise(this.state.exercise_value));
+    this.setState({exercise_value : ''});
+  }
+
+  render() {
+    return (
+      <div className = "exercise-page">
+        <form onSubmit = {(e) => this.calculateActivity(e)}>
+          <label htmlFor = "exercise">Enter your workout.  It can be anything from 30 min of weight lifting, 1 hour of yoga, or a 2 hour walk!</label><br/>
+          <textarea 
+            id = "exercise"
+            type = "textarea" 
+            placeholder = "ran 3 miles"
+            onChange = {(e) => this.exerciseInput(e)}
+            value = {this.state.exercise_value}
+          >
+          </textarea><br/>
+          <button type = "submit">Calculate Activity</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default connect()(ExerciseSearchPage);
