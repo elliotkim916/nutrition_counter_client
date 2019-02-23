810bcd45c9574df4666e0e98b7a9f35c093c6c6d
Elliot Kim <elliotkim916@gmail.com>
2019-02-22 18:52:34 -0800

    Updated renderResult with duration array and calories array, and added onAdd function

--- src/components/exercise-results.js
+++ src/components/exercise-results.js
@@ -2,22 +2,42 @@ import React, {Component} from 'react';
 import './exercise-results.css';
 import {connect} from 'react-redux';
 
-class ExerciseResults extends Component {
+export class ExerciseResults extends Component {
+  onAdd(e, cal, dur, username, date) {
+    e.preventDefault();
+    // Need to add code that will dispatch my async action that will make POST request to server
+  }
+
   renderResult() {
-    let sum;
+    let calorieSum;
+    let durationSum;
     let exercise_results_array = this.props.exerciseResults;
     let calories_burned_array = exercise_results_array.map(cal => {
       return cal.nf_calories;
     });
+    let total_duration_array = exercise_results_array.map(val => {
+      return val.duration_min;
+    });
     
-    if (calories_burned_array.length > 0) {
-      sum = calories_burned_array.reduce((acc, currentVal) => {
-        return acc + currentVal;
+    if (calories_burned_array.length && total_duration_array.length > 0) {
+      calorieSum = calories_burned_array.reduce((acc, currentVal) => {
+        return Math.floor(acc + currentVal);
+      });
+      durationSum = total_duration_array.reduce((acc, currentVal) => {
+        return Math.floor(acc + currentVal);
       });
 
       return (
-        <h3 className="cal-burned-sum">Total Calories Burned : {sum}</h3>
-      )
+        <div className = "exercise-totals">
+          <form
+            // onSubmit = {(e) => this.onAdd(e, cal, dur, this.props.username, Date.now)}
+          >
+            <h3>Total Calories Burned : {calorieSum}</h3>
+            <h3>Total Duration : {durationSum}</h3>
+            <button type = "submit">Save Exercise</button>
+          </form>
+        </div>
+      );
     }
   }
 
@@ -46,7 +66,8 @@ class ExerciseResults extends Component {
 }
 
 const mapStateToProps = state => ({
-  exerciseResults: state.exercise.exercise_results
+  exerciseResults: state.exercise.exercise_results,
+  username: state.auth.currentUser.username
 });
 
 export default connect(mapStateToProps)(ExerciseResults);
