7e88362decb1286c2f0e95b28657eb87e9300abf
Elliot Kim <elliotkim916@gmail.com>
2019-02-28 18:08:15 -0800

    Added exercise actions & addExercise async action

---
+++ src/actions/protected-exercise-data.js
@@ -0,0 +1,48 @@
+import {API_BASE_URL} from '../config.js';
+import {normalizeResponseErrors} from './utils.js';
+
+export const FETCH_EXERCISE_DATA = 'FETCH_EXERCISE_DATA';
+export const fetchExerciseData = data => ({
+  type : FETCH_EXERCISE_DATA,
+  data
+});
+
+export const FETCH_EXERCISE_ERROR = 'FETCH_EXERCISE_ERROR';
+export const fetchExerciseError = error => ({
+  type : FETCH_EXERCISE_ERROR,
+  error
+});
+
+export const ADD_EXERCISE_DATA = 'ADD_EXERCISE_DATA';
+export const addExerciseData = addedExerciseData => ({
+  type : ADD_EXERCISE_DATA,
+  addedExerciseData
+});
+
+export const DELETE_EXERCISE_DATA = 'DELETE_EXERCISE_DATA';
+export const deleteExerciseData = () => ({
+  type : DELETE_EXERCISE_DATA
+});
+
+export const addExercise = (calories, duration, username, date) => (dispatch, getState) => {
+  const authToken = getState().authReducer.authToken;
+  const body = JSON.stringify({
+    caloriesBurned : calories,
+    duration : duration,
+    username : username,
+    created : date
+  });
+  const headers = {
+    'Authorization' : `Bearer ${authToken}`,
+    'Content-Type' : 'application/json'
+  };
+
+  return fetch(`${API_BASE_URL}/exercise/${username}`, {
+    headers: headers,
+    body : body
+  })
+  .then(res => normalizeResponseErrors(res))
+  .then(res => res.json())
+  .then(data => dispatch(addExerciseData(data)))
+  .catch(err => dispatch(fetchExerciseError(err)))
+}
