02ee3b17ad29a0732db682651e5e2d1b171cfe18
Elliot Kim <elliotkim916@gmail.com>
2019-02-28 18:23:26 -0800

    Added exerciseDataReducer along with initialState

---
+++ src/reducers/protected-exercise-data.js
@@ -0,0 +1,34 @@
+import {
+  FETCH_EXERCISE_DATA,
+  FETCH_EXERCISE_ERROR,
+  ADD_EXERCISE_DATA,
+  DELETE_EXERCISE_DATA
+} from '../actions/protected-exercise-data';
+
+const initialState = {
+  exerciseData : '',
+  loading : false,
+  error : null
+};
+
+export function exerciseDataReducer(state=initialState, action) {
+  if (action.type === FETCH_EXERCISE_DATA) {
+    return Object.assign({}, state, {
+      exerciseData : action.data,
+      loading : false,
+      error : null
+    });
+  } else if (action.type === ADD_EXERCISE_DATA) {
+    return Object.assign({}, state, {
+      exerciseData : [...state.exerciseData, action.addedExerciseData],
+      loading : false,
+      error : null
+    });
+  } else if (action.type === FETCH_EXERCISE_ERROR) {
+    return Object.assign({}, state, {
+      error : action.error,
+      loading : false
+    });
+  }
+  return state;
+}
