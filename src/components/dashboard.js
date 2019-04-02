7e3488111ed74778063d7681ca396395665ff5dd
Elliot Kim <elliotkim916@gmail.com>
2019-04-02 11:36:19 -0700

    Changed log out to span element and added tabindex attribute

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -87,12 +87,7 @@ export class Dashboard extends React.Component {
       return (
         <div className="dashboard">
           <div className="shape">
-            <a 
-              className = "log-out"
-              onClick = {(e) => this.logOut(e)}
-            >
-            Log Out
-            </a><br/>
+            <span className="log-out" onClick={(e) => this.logOut(e)} tabindex="1">Log Out</span><br/>
             <h1 className="title-header">Nutrition Counter</h1>
             <NutritionSearchPage/>
             <ExerciseSearchPage/><br/>
