7980f670b75dbae86ca94dd6db6d9b62ea06c164
Elliot Kim <elliotkim916@gmail.com>
2019-04-17 12:19:19 -0700

    Removed console logs

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -42,7 +42,6 @@ const storeAuthInfo = (authToken, dispatch) => {
 
 export const login = (username, password) => dispatch => {
   dispatch(authRequest());
-  console.log(API_BASE_URL);
   return (
     fetch(`${API_BASE_URL}/auth/login`, {
       method: 'POST',
