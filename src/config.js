6c560df2204c76d612599f8335017062b0f6c818
Elliot Kim <elliotkim916@gmail.com>
2019-04-17 11:45:09 -0700

    Added localhost to API url

--- src/config.js
+++ src/config.js
@@ -3,5 +3,5 @@ export const EXERCISE_BASE_URL = "https://trackapi.nutritionix.com/v2/natural/ex
 export const APP_ID = "aeff1b26";
 export const APP_KEY = "96ef74633a183f547e97d2dc6c8819d4";
 
-// server side code
-export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
+// server side
+export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080/api';
