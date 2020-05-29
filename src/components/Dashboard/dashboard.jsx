2d4275b1b82498470d01d9378dbd21dd81d1d2af
Elliot Kim <elliotkim916@gmail.com>
2020-05-28 18:03:01 -0700

    Updated dashboard

--- src/components/Dashboard/dashboard.jsx
+++ src/components/Dashboard/dashboard.jsx
@@ -26,7 +26,7 @@ export class Dashboard extends React.Component {
     setTimeout(() => {
       this.props.dispatch(getProtectedData('nutrition'));
       this.props.dispatch(getProtectedData('exercise'));
-    }, 300);
+    }, 1000);
   }
 
   logOut() {
