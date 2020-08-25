8e096d2049294058b2162ed6f94d13078beeb7c8
Elliot Kim <elliotkim916@gmail.com>
2020-08-25 08:39:35 -0700

    Updated Loading component

--- src/components/Loading/loading.jsx
+++ src/components/Loading/loading.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 import '../../index.scss';
 import ClipLoader from 'react-spinners/ClipLoader';
 
-const Loading = props => {
+const Loading = React.memo(({ loadingMessage }) => {
   return (
     <div className="backdrop">
       <div className="backdrop-form">
-        <h3 className="loading-header">{props.loadingMessage}</h3>
-        <ClipLoader size={100} sizeUnit={"px"}/>
+        <h3 className="loading-header">{loadingMessage}</h3>
+        <ClipLoader size={100} sizeUnit={'px'} />
       </div>
     </div>
   );
-}
+});
 
 export default Loading;
