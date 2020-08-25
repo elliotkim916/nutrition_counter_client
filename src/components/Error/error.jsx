e656a13f3e1a0d3eaca0b1d54bd99c8f5fe5e4e1
Elliot Kim <elliotkim916@gmail.com>
2020-08-25 08:37:20 -0700

    Updated Error component

--- src/components/Error/error.jsx
+++ src/components/Error/error.jsx
@@ -1,23 +1,27 @@
 import React from 'react';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 import '../../index.scss';
 
-const Error = props => {
+const Error = ({ errorMessage, dispatch, clearError }) => {
   return (
     <div className="backdrop">
       <div className="backdrop-form">
-        <img src="https://img.icons8.com/clouds/100/000000/sad.png" alt="sad face"></img><br/>
-        <h3 className="error-message">{props.errorMessage}</h3>
-        <button 
-          type="button" 
-          className="login-btn" 
-          onClick={() => props.dispatch(props.clearError())}
+        <img
+          src="https://img.icons8.com/clouds/100/000000/sad.png"
+          alt="sad face"
+        ></img>
+        <br />
+        <h3 className="error-message">{errorMessage}</h3>
+        <button
+          type="button"
+          className="login-btn"
+          onClick={() => dispatch(clearError())}
         >
           Okay
         </button>
       </div>
-    </div> 
+    </div>
   );
-}
+};
 
-export default connect()(Error);
+export default connect()(React.memo(Error));
